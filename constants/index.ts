c55f5a91c3b1bbc5dcaf45f385dd66862ea1f716
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-03-31 12:37:48 +0100

    Add constant sidebarlink data

---
+++ constants/index.ts
@@ -0,0 +1,28 @@
+const sidebarLinks = [
+  {
+    label: 'Home',
+    route: '/',
+    imgURL: '/icons/Home.svg',
+  },
+  {
+    label: 'Upcoming',
+    route: '/upcoming',
+    imgURL: '/icons/upcoming.svg',
+  },
+  {
+    label: 'Previous',
+    route: '/previous',
+    imgURL: '/icons/Previous.svg',
+  },
+  {
+    label: 'Recordings',
+    route: '/recordings',
+    imgURL: '/icons/Video.svg',
+  },
+  {
+    label: 'Personal Room',
+    route: '/personal-room',
+    imgURL: '/icons/add-personal.svg',
+  },
+];
+export default sidebarLinks;
