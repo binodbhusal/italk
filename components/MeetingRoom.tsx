4224885108897cfbfb77de9175786b0205a9b0a2
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-04-09 22:34:47 +0100

    Update meeting room component

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -11,12 +11,14 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { LayoutList, Loader, Users } from 'lucide-react';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, useRouter } from 'next/navigation';
 import EndCallButton from './EndCallButton';
+
 
 type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 const MeetingRoom = () => {
     const searchParams = useSearchParams();
+    const router = useRouter();
   const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
   const [showParticipants, setShowParticipants] = useState(false);
   const isePersonalRoom = !!searchParams.get('personal');
@@ -48,7 +50,7 @@ const MeetingRoom = () => {
 
       </div>
       <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
-        <CallControls />
+        <CallControls onLeave={() => router.push('/')} />
         <DropdownMenu>
           <div className="flex items-center">
             <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
